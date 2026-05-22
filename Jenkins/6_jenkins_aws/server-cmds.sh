e0885c62f5afb9ea51f50c4f8db15cb54ca0e482
Babak-H <babak.habibnejad@gmail.com>
2026-05-22 14:07:28 -0500

    updated jenkins files

--- Jenkins/6_jenkins_aws/server-cmds.sh
+++ Jenkins/6_jenkins_aws/server-cmds.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # read the first parameter given to this file
-export IMAGE=$1
-docker-compose -f docker-compose.yml up --detach
+export IMAGE="${1:?Image name is required}"
+
+docker-compose -f docker-compose.yml pull
+docker-compose -f docker-compose.yml up --detach --remove-orphans
 echo "success"
